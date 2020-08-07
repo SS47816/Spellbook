b0ffbd32a58cf5f20000867db9aa4b63e3fef15e
SS47816 <ss1057532022@gmail.com>
2020-08-07 13:23:48 +0800

    feat: use np.full and np.arange

--- uv/src/plot.py
+++ uv/src/plot.py
@@ -4,30 +4,28 @@ import matplotlib.animation as animation
 
 class UVTube:
 
-    def __init__(self, length, height, sections, intensity, speed):
+    def __init__(self, length, height, sections, intensity, x_start, speed):
         self.length_ = length
         self.height_ = height
         self.sections_ = int(sections)
         self.intensity_ = intensity
         self.speed_ = speed
-        self.x_ = 0     # horizontal position
-        self.y_ = np.empty(sections, dtype=np.float)
-        for i in range(0, sections):
-            self.y_[i] = i*length/sections + height
+        self.x_ = x_start                               # horizontal position
+        self.y_ = np.arange(start=height, stop=length+height, step=length/sections, dtype=np.float)
 
     def move(self, dt):
         self.x_ = self.x_ + self.speed_*dt
 
 class Wall:
 
-    def __init__(self, length, height, mesh_size, dist_to_wall):
+    def __init__(self, length, height, mesh_size, dist_to_wall, init_dose):
         self.length_ = length
         self.height_ = height
         self.mesh_size_ = mesh_size
         self.n_x_ = int(length/mesh_size)
         self.n_y_ = int(height/mesh_size)
         self.z_ = 2.0
-        self.distribution = np.zeros(shape=(self.n_y_, self.n_x_), dtype=np.float)
+        self.distribution = np.full((self.n_y_, self.n_x_), init_dose, dtype=np.float)
 
     def calcDist(self, tube_x, tube_y, i, j, z):
         x = i*self.mesh_size_ - tube_x
@@ -46,10 +44,11 @@ class Wall:
 
 def main():
     print("Running Simulation...")
-    wall_length = 10.0
+    wall_length = 5.0
     wall_height = 3.0
     wall_mesh_size = 0.1
     dist_to_wall = 2.0
+    init_dose = -25.0
 
     tube_length = 1.0
     tube_height = 0.65
@@ -57,11 +56,12 @@ def main():
     tube_intensity = 1.83
     tube_speed = 0.05
 
-    wall = Wall(wall_length, wall_height, wall_mesh_size, dist_to_wall)
-    tube = UVTube(tube_length, tube_height, tube_n_sections, tube_intensity, tube_speed)
-
     dt = 0.1
-    t_steps = int(wall_length/tube_speed/dt)
+    x_start = 0.0
+    x_end = 5.0
+
+    wall = Wall(wall_length, wall_height, wall_mesh_size, dist_to_wall, init_dose)
+    tube = UVTube(tube_length, tube_height, tube_n_sections, tube_intensity, x_start, tube_speed)
 
     fig, ax = plt.subplots()
     ax.set_xlabel('Wall width (x0.1m)')
@@ -69,14 +69,14 @@ def main():
     ax.set_title('Total Dose Recieved by the Wall (mJ)')
     ims = []
 
-    for t in range(t_steps):
+    while tube.x_ <= x_end:
         wall.recieveDose(tube, dt)
         tube.move(dt)
 
         im = ax.imshow(wall.distribution, cmap='hot', interpolation='nearest', animated=True)
         ims.append([im])
     
-    ani = animation.ArtistAnimation(fig, ims, interval=0, repeat_delay=3000, blit=True)
+    ani = animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=1000, blit=True)
     plt.show()
 
 if __name__ == "__main__":
